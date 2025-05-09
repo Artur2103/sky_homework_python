3fb8494383c778525cccb978b828dee789ab6954
Artur <artur.yafasov@gmail.com>
2025-05-09 15:58:23 +0300

    1

--- lesson_3/lesson_3_task_3.py
+++ lesson_3/lesson_3_task_3.py
@@ -6,9 +6,6 @@ to_address = Address(222222, "Moscow", "Pusnkin str", 2, 2)
 
 mailing = Mailing(from_address, to_address, 5000, 9999)
 
-#print(f"Отправление {mailing.track} из {from_address.index}.{from_address.city}.{from_address.street}."
- #     f"{from_address.block}-{from_address.apartment} в {to_address.index}.{to_address.city}."
-  #    f"{to_address.street}.{to_address.block}-{to_address.apartment}. Стоимость {mailing.cost} рублей.")
-
-print(f"Отправление {mailing.track} из from_address в {to_address.index}.{to_address.city}."
+print(f"Отправление {mailing.track} из {from_address.index}.{from_address.city}.{from_address.street}."
+     f"{from_address.block}-{from_address.apartment} в {to_address.index}.{to_address.city}."
       f"{to_address.street}.{to_address.block}-{to_address.apartment}. Стоимость {mailing.cost} рублей.")
