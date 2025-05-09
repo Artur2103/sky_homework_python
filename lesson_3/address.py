e828bd68eac9d6a7f93e949edb7ed9dfc6ed85c2
Artur <artur.yafasov@gmail.com>
2025-05-09 14:21:45 +0300

    lesson_3

---
+++ lesson_3/address.py
@@ -0,0 +1,7 @@
+class Address:
+    def __init__(self, index, city, street, block, apartment):
+        self.index = index
+        self.city = city
+        self.street = street
+        self.block = block
+        self.apartment = apartment
