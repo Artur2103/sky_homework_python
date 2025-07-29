5a4ae68bcc299308ce40c5c4927456588cc6f082
Artur <artur.yafasov@gmail.com>
2025-07-29 14:03:11 +0300

    6k

--- lesson_6k/test_01_form.py
+++ lesson_6k/test_01_form.py
@@ -2,10 +2,10 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service as EdgeService
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
-from selenium.webdriver.edge.options import Options as EdgeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 
 @pytest.fixture
 def driver():
@@ -17,35 +17,60 @@ def driver():
     yield driver
     driver.quit()
 
+
 def test_fill_form(driver):
     # Переход на страницу
-    driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
+    driver.get(
+        "https://bonigarcia.dev/selenium-webdriver-java/data-types.html"
+        )
 
     wait = WebDriverWait(driver, 10)
 
     # Заполнение формы
-    driver.find_element(By.CSS_SELECTOR, '[name=first-name]').send_keys('Иван')
-    driver.find_element(By.CSS_SELECTOR, '[name=last-name]').send_keys('Петров')
-    driver.find_element(By.CSS_SELECTOR, '[name=address]').send_keys('Ленина, 55-3')
-    driver.find_element(By.CSS_SELECTOR, '[name=e-mail]').send_keys('test@skypro.com')
-    driver.find_element(By.CSS_SELECTOR, '[name=phone]').send_keys('+7985899998787')
-    driver.find_element(By.CSS_SELECTOR, '[name=zip-code]').send_keys('')
-    driver.find_element(By.CSS_SELECTOR, '[name=city]').send_keys('Москва')
-    driver.find_element(By.CSS_SELECTOR, '[name=country]').send_keys('Россия')
-    driver.find_element(By.CSS_SELECTOR, '[name=job-position]').send_keys('QA')
-    driver.find_element(By.CSS_SELECTOR, '[name=company]').send_keys('SkyPro')
+    driver.find_element(
+        By.CSS_SELECTOR, '[name=first-name]').send_keys('Иван')
+    driver.find_element(
+        By.CSS_SELECTOR, '[name=last-name]').send_keys('Петров')
+    driver.find_element(
+        By.CSS_SELECTOR, '[name=address]').send_keys('Ленина, 55-3')
+    driver.find_element(
+        By.CSS_SELECTOR, '[name=e-mail]').send_keys('test@skypro.com')
+    driver.find_element(
+        By.CSS_SELECTOR, '[name=phone]').send_keys('+7985899998787')
+    driver.find_element(
+        By.CSS_SELECTOR, '[name=zip-code]').send_keys('')
+    driver.find_element(
+        By.CSS_SELECTOR, '[name=city]').send_keys('Москва')
+    driver.find_element(
+        By.CSS_SELECTOR, '[name=country]').send_keys('Россия')
+    driver.find_element(
+        By.CSS_SELECTOR, '[name=job-position]').send_keys('QA')
+    driver.find_element(
+        By.CSS_SELECTOR, '[name=company]').send_keys('SkyPro')
 
     # Ждем, пока кнопка станет кликабельной
-    submit_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[type=submit]")))
+    submit_button = wait.until(EC.element_to_be_clickable((
+        By.CSS_SELECTOR, "[type=submit]")))
 
     driver.execute_script("arguments[0].click();", submit_button)
-      
+
     # Проверка подсветки поля Zip code
-    pole_zip_code = driver.find_element(By.ID, "zip-code").get_attribute("class")
+    pole_zip_code = driver.find_element(
+        By.ID, "zip-code").get_attribute("class")
     assert pole_zip_code == "alert py-2 alert-danger"
 
     # Проверка подсветки остальных полей
-    poles = ["#first-name", "#last-name", "#address", "#city", "#country", "#e-mail", "#phone", "#company"]
+    poles = [
+        "#first-name",
+        "#last-name",
+        "#address",
+        "#city",
+        "#country",
+        "#e-mail",
+        "#phone",
+        "#company"
+        ]
     for pole in poles:
-        pole_class = driver.find_element(By.CSS_SELECTOR, pole).get_attribute("class")
+        pole_class = driver.find_element(
+            By.CSS_SELECTOR, pole).get_attribute("class")
         assert pole_class == "alert py-2 alert-success"
--- lesson_6k/test_03_shop.py
+++ lesson_6k/test_03_shop.py
@@ -2,10 +2,8 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 
 @pytest.fixture
 def driver():
@@ -17,33 +15,44 @@ def driver():
     yield driver
     driver.quit()
 
+
 def test_calc(driver):
     # Переход на страницу
     driver.get("https://www.saucedemo.com")
 
-    wait = WebDriverWait(driver, 50)
-
     # Заполнение полей авторизации + нажатие кнопки Login
-    driver.find_element(By.CSS_SELECTOR, "#user-name").send_keys("standard_user")
-    driver.find_element(By.CSS_SELECTOR, "#password").send_keys("secret_sauce")
-    driver.find_element(By.CSS_SELECTOR, "#login-button").click()
+    driver.find_element(
+        By.CSS_SELECTOR, "#user-name").send_keys("standard_user")
+    driver.find_element(
+        By.CSS_SELECTOR, "#password").send_keys("secret_sauce")
+    driver.find_element(
+        By.CSS_SELECTOR, "#login-button").click()
 
     # Добавление товаров в корзину
-    driver.find_element(By.CSS_SELECTOR, "#add-to-cart-sauce-labs-backpack").click()
-    driver.find_element(By.CSS_SELECTOR, "#add-to-cart-sauce-labs-bolt-t-shirt").click()
-    driver.find_element(By.CSS_SELECTOR, "#add-to-cart-sauce-labs-onesie").click()
+    driver.find_element(
+        By.CSS_SELECTOR, "#add-to-cart-sauce-labs-backpack").click()
+    driver.find_element(
+        By.CSS_SELECTOR, "#add-to-cart-sauce-labs-bolt-t-shirt").click()
+    driver.find_element(
+        By.CSS_SELECTOR, "#add-to-cart-sauce-labs-onesie").click()
 
     # Переход в корзину + нажатие кнопки checkout
-    driver.find_element(By.CSS_SELECTOR, ".shopping_cart_link").click()
-    driver.find_element(By.CSS_SELECTOR, "#checkout").click()
+    driver.find_element(
+        By.CSS_SELECTOR, ".shopping_cart_link").click()
+    driver.find_element(
+        By.CSS_SELECTOR, "#checkout").click()
 
     # Заполнение формы данными + нажатие кнопки Continue
-    driver.find_element(By.CSS_SELECTOR, "#first-name").send_keys("Ivan")
-    driver.find_element(By.CSS_SELECTOR, "#last-name").send_keys("Petrov")
-    driver.find_element(By.CSS_SELECTOR, "#postal-code").send_keys("123456")
-    driver.find_element(By.CSS_SELECTOR, "#continue").click()
+    driver.find_element(
+        By.CSS_SELECTOR, "#first-name").send_keys("Ivan")
+    driver.find_element(
+        By.CSS_SELECTOR, "#last-name").send_keys("Petrov")
+    driver.find_element(
+        By.CSS_SELECTOR, "#postal-code").send_keys("123456")
+    driver.find_element(
+        By.CSS_SELECTOR, "#continue").click()
 
     # Проверка итоговой суммы
-    total = driver.find_element(By.CSS_SELECTOR, ".summary_total_label").text
+    total = driver.find_element(
+        By.CSS_SELECTOR, ".summary_total_label").text
     assert total == "Total: $58.29"
-    
